1ba56e5d69cb10cf2ef258160fcf3fb9418983f0
chesdenis <chesdenis@gmail.com>
2016-09-02 23:33:30 +0300

    changed components code

---
+++ PFS.Server.MvcApp/Scripts/app.module.ts
@@ -0,0 +1,11 @@
+﻿import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './cmps/app.component';
+import { ChildComponent } from './cmps/child.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent, ChildComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
