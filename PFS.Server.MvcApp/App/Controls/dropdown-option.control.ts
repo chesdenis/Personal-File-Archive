762642eca32d3a7522556a7df8b91cd122bcde0a
ivanblindyuk <ivanblindyuk@mail.ru>
2016-09-07 23:46:45 +0300

    Resolve bug with reading content from dropdown-option

--- PFS.Server.MvcApp/App/Controls/dropdown-option.control.ts
+++ PFS.Server.MvcApp/App/Controls/dropdown-option.control.ts
@@ -1,14 +1,19 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, ElementRef} from "@angular/core";
 import {DropdownControl} from "./dropdown.control";
 
 @Component({
     selector: "dropdown-option",
     templateUrl: "./dropdown-option.control.html"
 })
-export class DropdownOptionControl{
-    @Input() value:string;
+export class DropdownOptionControl {
+    @Input() value: string;
+    content: string;
 
-    constructor(dropdown:DropdownControl){
+    constructor(dropdown: DropdownControl, private elRef: ElementRef) {
         dropdown.addOption(this);
+    }
+
+    ngAfterContentChecked() {
+        this.content = this.elRef.nativeElement.textContent;
     }
 }
