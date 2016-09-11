6e7b6132d5ae274252afb0fddbc4ac32ba47064c
chesdenis <chesdenis@gmail.com>
2016-09-11 12:59:52 +0300

    included menu model and changed header visibility logic

--- PFS.Server.MvcApp/App/Controls/nav-bar/nav-bar-zone.control.ts
+++ PFS.Server.MvcApp/App/Controls/nav-bar/nav-bar-zone.control.ts
@@ -1,4 +1,6 @@
 ﻿import {Component, Input} from '@angular/core';
+import {NavBarMenuEntity} from './nav-bar-menu.entity';
+import {NavTopBarControl} from './nav-top-bar.control';
 
 @Component({
     selector: 'nav-bar-zone',
@@ -7,11 +9,19 @@
 export class NavBarZoneControl
 {
     @Input() align: string = "left";
+    @Input() menuEntities: NavBarMenuEntity[] = [];
+
+    constructor(private navBar: NavTopBarControl) {
+    }
     
     getClasses() {
         return {
             'navbar-left': this.align === "left",
             'navbar-right': this.align === "right"
         }
+    }
+
+    onSelectMenuEntity() {
+        this.navBar.hideBody();
     }
 }
--- PFS.Server.MvcApp/App/app.component.ts
+++ PFS.Server.MvcApp/App/app.component.ts
@@ -1,6 +1,25 @@
 import { Component } from '@angular/core';
+import {NavBarMenuEntity} from './Controls/nav-bar/nav-bar-menu.entity';
+
 @Component({
   selector: 'my-app',
   templateUrl:"./app.component.html",
 })
-export class AppComponent { }  
+export class AppComponent 
+{
+    leftMenuEntities: NavBarMenuEntity[] = [
+        new NavBarMenuEntity("active", "/All", "All", "(current)" ),
+        new NavBarMenuEntity("active", "/Photos", "Photos", "(current)"),
+        new NavBarMenuEntity("active", "/Videos", "Videos", "(current)"),
+        new NavBarMenuEntity("active", "/Musics", "Musics", "(current)"),
+        new NavBarMenuEntity("active", "/Books", "Books", "(current)"),
+        new NavBarMenuEntity("active", "/Documents", "Documents", "(current)"),
+        new NavBarMenuEntity("active", "/Others", "Others", "(current)"),
+        new NavBarMenuEntity("active", "/Upload", "Upload", "(current)"),
+        new NavBarMenuEntity("active", "/Untagged", "Untagged", "(current)"),
+    ];
+
+    rightMenuEntities: NavBarMenuEntity[] = [
+        new NavBarMenuEntity("active", "/Settings", "Settings", "(current)")
+    ];
+}  
