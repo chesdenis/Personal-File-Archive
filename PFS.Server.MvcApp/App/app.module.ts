84dc8fb628577af0bb6470a0115508b0158e1156
NomerovskiySergey <nomerushka@gmail.com>
2016-09-13 00:13:13 +0300

    Создал контрол Filters соласно таске Controls.Filters #30

--- PFS.Server.MvcApp/App/app.module.ts
+++ PFS.Server.MvcApp/App/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import {ChildComponent} from './Components/child.component';
 
+import {ContentFilterControl} from './Controls/Filters/content-filter.control'
+import {FiltersOptionControl} from './Controls/Filters/filters-option.control'
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [AppComponent, ChildComponent ],
+  declarations: [AppComponent, ChildComponent, ContentFilterControl, FiltersOptionControl ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
