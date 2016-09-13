9f37f8dd3acc10dcf89b2362a869618b079aaa45
KiselyovaTatyana <kiseleva.t@outlook.com>
2016-09-13 10:51:21 +0300

    added tab.control

--- PFS.Server.MvcApp/App/app.module.ts
+++ PFS.Server.MvcApp/App/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import {ChildComponent} from './Components/child.component';
 
+import {TabsControl} from './Controls/tabs.control'
+import {TabControl} from './Controls/tab.control'
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [AppComponent, ChildComponent ],
+  declarations: [AppComponent, ChildComponent,TabsControl, TabControl],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
