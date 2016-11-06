b2e619e4105219e0f52200ac38a2d0221cf4232c
chesdenis <chesdenis@gmail.com>
2016-11-06 19:44:58 +0300

    created odata controller for books, mock context and netcore odata project

---
+++ PFS.Server.Repository.NetCoreOData/Mock/IMockContext.cs
@@ -0,0 +1,19 @@
+﻿using PFS.Server.Repository.NetCoreOData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFS.Server.Repository.NetCoreOData.Mock
+{
+    public interface IMockContext
+    {
+        IEnumerable<Book> Books { get; }
+        IEnumerable<Document> Documents { get; }
+        IEnumerable<Music> Musics { get; }
+        IEnumerable<Photo> Photos { get; }
+        IEnumerable<Video> Videos { get; }
+     
+
+    }
+}
---
+++ PFS.Server.Repository.NetCoreOData/Model/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFS.Server.Repository.NetCoreOData.Model
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
