0a3419360b09c41c6798c390414166f3113cbbdb
NomerovskiySergey <nomerushka@gmail.com>
2016-11-10 20:32:42 +0200

    commit typeahead

--- PFS.Server.MvcApp/App/Controls/Typeahead/typeahead.control.ts
+++ PFS.Server.MvcApp/App/Controls/Typeahead/typeahead.control.ts
@@ -1,12 +1,14 @@
 import {Component, Input} from "@angular/core"
 import {SearchInputControl} from "./search-input.control"
 import {TypeaheadEntity} from "./typeahead-entity"
+import {TypeaheadSearchService} from '../../Services/TypeaheadService/typeahead-search.service';
 
 @Component({
     moduleId: module.id,
     selector: "typeahead",
     templateUrl: './typeahead.control.html',
     styleUrls: ["./typehead.control.css"],
+    providers: [TypeaheadSearchService]
 })
 export class TypeaheadControl {
 
@@ -14,11 +16,12 @@ export class TypeaheadControl {
     protected selectedValue: string = "";
     protected typeaheadEntity : TypeaheadEntity;
 
+    constructor(private searchService: TypeaheadSearchService) { }
 
     protected  SearchProcess(value: any) : void {
 
         // Send request to server
-        this.searchDataArr = ["member", "TypeScript", "Finished", "watch", "ts", "multi", "after"];
+        this.searchDataArr = this.searchService.getMoqSearchData();
     }
 
     protected SelectItem(event: any) : void{ 
--- PFS.Server.MvcApp/App/Services/TypeaheadService/typeahead-search.service.ts
+++ PFS.Server.MvcApp/App/Services/TypeaheadService/typeahead-search.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+
 import { DataFromServer } from "./typeahead-mock"
 
 @Injectable()
 export class TypeaheadSearchService {
-     getSearchData(): string[] {
+
+    constructor (private http: Http) {}
+
+     getMoqSearchData(): string[] {
          return DataFromServer;
      } 
 }
