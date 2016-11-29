3b76e9250c00e4f217d094487410cc6a396fc26a
NomerovskiySergey <nomerushka@gmail.com>
2016-11-29 22:28:04 +0200

    Change type of project PFS.Server.EntityFramework.OData

---
+++ PFS.Server.EntityFramework.OData/PFS.Server.EntityFramework.OData/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using PFS.Server.EntityFramework.OData.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace PFS.Server.EntityFramework.OData
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Product>("Products");
+            builder.EntitySet<Photo>("Photo");
+
+            config.MapODataServiceRoute("ODataRoute", "service", builder.GetEdmModel());
+        }
+    }
+}
---
+++ PFS.Server.EntityFramework.OData/PFS.Server.EntityFramework.OData/Models/Photo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PFS.Server.EntityFramework.OData.Models
+{
+    public class Photo : Entity             
+    {
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
