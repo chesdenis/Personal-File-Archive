eb8b1de00fb0483786d7515e3e9608c79a9680dc
chesdenis <chesdenis@gmail.com>
2016-12-06 06:46:14 +0000

    apply Pfs.Server.Core to  PFS.Server.DbProvider.EfCore.SqLiteOData

---
+++ PFS.Server.DbProvider.EfCore.SqLiteOData/Db/PfsServerDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PFS.Server.Core.Abstractions;
+using PFS.Server.Core.Entities;
+using PFS.Server.DbProvider.EfCore.SqLiteOData.Db.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFS.Server.DbProvider.EfCore.SqLiteOData.Db
+{
+    public partial class PfsServerDbContext : DbContext, IPfsDbContext
+    {
+        public DbSet<Tag> Tags { get; set; }
+        IEnumerable<Tag> IPfsODataCollections.Tags => Tags;
+
+        public IPfsDataProvider<Tag> TagsProvider { get; set; }
+
+        public PfsServerDbContext()
+        {
+            TagsProvider = new TagsProvider(Tags);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Tag>().HasKey(m => m.Id);
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./PfsServer.db");
+        }
+
+        void IPfsDbContext.SaveChanges()
+        {
+            SaveChanges();
+        }
+    }
+}
--- PFS.Server.DbProvider.EfCore.SqLiteOData/Startup.cs
+++ PFS.Server.DbProvider.EfCore.SqLiteOData/Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PFS.Server.Core.Abstractions;
+using PFS.Server.Core.Entities;
+using PFS.Server.Core.Repositories;
+using Microsoft.AspNetCore.OData.Extensions;
+using PFS.Server.DbProvider.EfCore.SqLiteOData.Db;
 
 namespace PFS.Server.DbProvider.EfCore.SqLiteOData
 {
@@ -16,6 +21,13 @@ namespace PFS.Server.DbProvider.EfCore.SqLiteOData
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            
+            services.AddOData<IPfsODataCollections>();
+            services.AddDbContext<PfsServerDbContext>();
+            services.AddLogging();
+            services.AddScoped<IPfsRepository<Tag>, TagsRepository>();
+            services.AddScoped<IPfsDbContext>(provider => provider.GetService<PfsServerDbContext>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,15 +35,10 @@ namespace PFS.Server.DbProvider.EfCore.SqLiteOData
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            loggerFactory.AddDebug();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseOData("odata");
+            app.UseMvc();
         }
     }
 }
