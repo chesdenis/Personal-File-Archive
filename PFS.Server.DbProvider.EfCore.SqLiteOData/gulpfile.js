766e8a7548ab6fa821d46299695da1d4b2372901
DC <DC@WIN2008R2>
2017-01-30 05:22:07 +0000

    changed commands for sqlite database, included hosting configurations for dotnet core apps

--- PFS.Server.DbProvider.EfCore.SqLiteOData/gulpfile.js
+++ PFS.Server.DbProvider.EfCore.SqLiteOData/gulpfile.js
@@ -1,7 +1,7 @@
 ﻿var exec = require('child_process').exec;
 var gulp = require('gulp');
 
-gulp.task('dotnet-ef-database-update', function (cb) {
+gulp.task('dotnet-ef-database-create', function (cb) {
     exec('dotnet ef database update', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -25,16 +25,7 @@ gulp.task('dotnet-ef-dbcontext-list', function (cb) {
     });
 });
 
-gulp.task('dotnet-ef-migrations-addonly', function (cb) {
-
-    exec('dotnet ef migrations add "PfsDbMigration"', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-});
-
-gulp.task('dotnet-ef-migrations-add', ['dotnet-ef-migrations-remove'], function (cb) {
+gulp.task('dotnet-ef-migrations-add', function (cb) {
 
     exec('dotnet ef migrations add "PfsDbMigration"', function (err, stdout, stderr) {
         console.log(stdout);
