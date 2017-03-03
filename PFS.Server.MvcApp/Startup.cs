1205e780dc332a1f25bf487d1f37a313f587342f
chesdenis <chesdenis@gmail.com>
2017-03-03 06:24:06 +0000

    investigate static content with different sources

--- PFS.Server.MvcApp/Startup.cs
+++ PFS.Server.MvcApp/Startup.cs
@@ -6,11 +6,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using PFS.Server.MvcApp.Extensions;
 
 namespace PFS.Server.MvcApp
 {
     public class Startup
     {
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,17 +27,13 @@ namespace PFS.Server.MvcApp
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
+            app.UseStaticFolders();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-
-                //routes.MapRoute(
-                //    name: "AllContentsRouting",
-                //    template: "App/Components/AllContents/all-contents.module",
-                //    defaults:new { controller = "Home", action = "Index" });
             });
         }
     }
