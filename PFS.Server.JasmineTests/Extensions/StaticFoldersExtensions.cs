a6495b9970fa22282bb43eb87bb01423db8d8acb
chesdenis <chesdenis@gmail.com>
2017-03-07 06:01:13 +0000

    created blank jasmine test environment

---
+++ PFS.Server.JasmineTests/Extensions/StaticFoldersExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFS.Server.JasmineTests.Extensions
+{
+    public static class StaticFoldersExtensions
+    {
+        public static IApplicationBuilder UseJasmine(this IApplicationBuilder app)
+        {
+            var pathToJasmine = Path.Combine(Directory.GetCurrentDirectory(), "Jasmine");
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(pathToJasmine),
+                RequestPath = new PathString("/jasmine")
+            });
+             
+            return app;
+        }
+    }
+}
