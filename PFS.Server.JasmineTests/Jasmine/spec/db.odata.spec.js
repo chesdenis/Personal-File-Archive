87796960d8627c64f8fe7c40eb220e4b08e85cf6
chesdenis <chesdenis@gmail.com>
2017-03-10 06:07:50 +0000

    separate actions for testing

--- PFS.Server.JasmineTests/Jasmine/spec/db.odata.spec.js
+++ PFS.Server.JasmineTests/Jasmine/spec/db.odata.spec.js
@@ -1,29 +1,12 @@
 ﻿describe("MSSqlServer with Entity Framework api tests", function () {
-    var apiUrl = "http://localhost:64245/odata";
+    //var apiUrl = "http://localhost:64245/odata";
+    var apiUrl = "http://localhost:5020/odata";
 
-    describe("should be able to create new Tag", shouldCreateTag);
-    describe("should be able to read all tags", shouldReadAllTags);
-
-    function shouldCreateTag() {
-
-        var isDone = false;
-        
-        beforeEach(function(done){
-            createTag().then(function () {
-                isDone = true; done();
-            }).catch(function () {
-                isDone = false; done();
-            });
-        });
-
-        it("Ok?", () => { expect(isDone).toEqual(true); });
-    };
-
-    function shouldReadAllTags() {
+    function should(context, funcName) {
         var isDone = false;
 
         beforeEach(function (done) {
-            readAllTags().then(function () {
+            context[funcName]().then(function () {
                 isDone = true; done();
             }).catch(function () {
                 isDone = false; done();
@@ -33,24 +16,41 @@
         it("Ok?", () => { expect(isDone).toEqual(true); });
     }
 
-    function readAllTags() {
-        return new Promise(function (resolve, reject) {
-            $data.service(apiUrl, function (contextFactory, contextType) {
-                var ctx = contextFactory();
-                resolve(ctx.Tags.toArray());
-            });
-        });
-    }
+    var actionsToTest = new dbOdataActions();
+    actionsToTest.init(apiUrl);
 
-    function createTag() {
-        return new Promise(function (resolve, reject) {
-            $data.service(apiUrl, function (contextFactory, contextType) {
-                var ctx = contextFactory();
-                ctx.Tags.add(new ctx.Tags.Tag({ Name: 'Tag from jasmine' }));
+    //describe("should be able to create new Tag", shouldCreateTag);
+    describe("should be able to read all tags", function () {
+        should(actionsToTest, "readAllTags");
+    });
 
-                resolve(ctx.saveChanges());
-            });
-        });        
-    };
+    //function shouldCreateTag() {
+
+    //    var isDone = false;
+        
+    //    beforeEach(function(done){
+    //        createTag().then(function () {
+    //            isDone = true; done();
+    //        }).catch(function () {
+    //            isDone = false; done();
+    //        });
+    //    });
+
+    //    it("Ok?", () => { expect(isDone).toEqual(true); });
+    //};
+
+    //function shouldReadAllTags() {
+    //    var isDone = false;
+
+    //    beforeEach(function (done) {
+    //        readAllTags().then(function () {
+    //            isDone = true; done();
+    //        }).catch(function () {
+    //            isDone = false; done();
+    //        });
+    //    });
+
+    //    it("Ok?", () => { expect(isDone).toEqual(true); });
+    //}
 
 });
---
+++ PFS.Server.JasmineTests/Jasmine/src/db.odata.actions.js
@@ -0,0 +1,43 @@
+﻿function dbOdataActions() {
+    this.init = function (apiUrl) {
+        this.apiUrl = apiUrl;
+    };
+    this.should = function (funcToCheck) {
+        var isDone = false;
+
+        beforeEach(function (done) {
+            funcToCheck().then(function () {
+                isDone = true; done();
+            }).catch(function () {
+                isDone = false; done();
+            });
+        });
+
+        it("Ok?", () => { expect(isDone).toEqual(true); });
+    };
+
+    this.readAllTags = function () {
+        var apiUrl = this.apiUrl;
+
+        return new Promise(function (resolve, reject) {
+            $data.service(apiUrl, function (contextFactory, contextType) {
+                var ctx = contextFactory();
+                resolve(ctx.Tags.toArray());
+            });
+        });
+    };
+    this.createTag = function () {
+        var apiUrl = this.apiUrl;
+
+        return new Promise(function (resolve, reject) {
+            $data.service(apiUrl, function (contextFactory, contextType) {
+                var ctx = contextFactory();
+                ctx.Tags.add(new ctx.Tags.Tag({ Name: 'Tag from jasmine' }));
+
+                resolve(ctx.saveChanges());
+            });
+        });
+    };
+
+
+};
