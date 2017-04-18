aff2c2bbc81272f27954e342dddf0ef7e9d4ee2f
chesdenis <chesdenis@gmail.com>
2017-04-18 18:52:33 +0000

    scan content in static folders job

--- PFS.Server.Core/Jobs/ScanPhotosInStaticFoldersJob.cs
+++ PFS.Server.Core/Jobs/ScanPhotosInStaticFoldersJob.cs
@@ -4,6 +4,7 @@ using System.Text;
 using PFS.Server.Core.DbContexts;
 using PFS.Server.Core.Entities;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace PFS.Server.Core.Jobs
 {
@@ -17,6 +18,28 @@ namespace PFS.Server.Core.Jobs
         public override void Execute(Job jobInDb, PfsServerDbContext dbCtx)
         {
             var contentSources = dbCtx.ContentSources.ToList();
+        }
+    }
+
+    public class ScanContentSource : BaseJob
+    {
+        [DataContract]
+        public class Args : BaseArgs
+        {
+            [DataMember]
+            public string ContentSourcePath { get; }
+            [DataMember]
+            public string ContentSourceName { get; set; }
+        }
+
+        public override BaseArgs DeserializeArgs(string argsAsJson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(Job jobInDb, PfsServerDbContext dbCtx)
+        {
+            throw new NotImplementedException();
         }
     }
 }
